0fdb32f7760528506c793a1f6edfea0949a0cf54
Johnny <johnny055238@gmail.com>
2019-09-22 14:43:21 +0800

    simultaneously progressing

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
-from flask import Flask, render_template, request
+import os
+import requests
 import random
 
+from flask import Flask, jsonify, render_template, request
+from flask_socketio import SocketIO, emit
+
 app = Flask(__name__)
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+socketio = SocketIO(app)
+
+clicked = []
+blue = 9
+red = 8
 
 @app.route("/")
 def index():
@@ -23,4 +33,24 @@ def index():
 def go():
     w = request.args.getlist("w")
     c = request.args.getlist("c")
-    return render_template("main.html",w=w, c=c)
+    return render_template("main.html",w=w,c=c,clicked=clicked,blue=blue,red=red)
+
+
+@socketio.on("on click")
+def click(data1, data2, data3):
+    clicked.append(data1["info"])
+    global blue, red
+    blue -= data2["blue"]
+    red -= data3["red"]
+    emit("do things", {'clicked':clicked, 'blue':blue,'red':red}, broadcast=True)
+
+@socketio.on("clear")
+def clear():
+    clicked.clear()
+    global blue, red
+    blue = 9
+    red = 8
+    emit("clear all", broadcast=True)
+
+
+
